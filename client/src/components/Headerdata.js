9c2fcbebb9ad6402e606f8d3e24cb08e4def599a
AbhayChandel13 <chandelabhay4515@gmail.com>
2022-09-13 20:29:04 +0530

    comment removed

--- client/src/components/Headerdata.js
+++ client/src/components/Headerdata.js
@@ -1,17 +1,13 @@
 export default function Headerdata({ handlePrint }) {
   return (
     <>
-     
-        <header className="flex flex-col items-center justify-center mb-2 xl:flex-row xl:justify-between">
-          <div>
-            <h1 className="font-bold uppercase tracking-wide text-2xl mb-2">
-              Invoicer
-            </h1>
-          </div>
-        </header>
-
-      //commnet Added in Headerdata file 
-    
+      <header className="flex flex-col items-center justify-center mb-2 xl:flex-row xl:justify-between">
+        <div>
+          <h1 className="font-bold uppercase tracking-wide text-2xl mb-2">
+            Invoicer
+          </h1>
+        </div>
+      </header>
     </>
   );
 }
